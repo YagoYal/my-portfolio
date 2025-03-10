927230d413854fb3d08c11c9cad40f4953baf8ab
YagoYal <yal.yep@gmail.com>
2025-03-10 14:26:22 -0300

    feat: configs material ui theme and css baseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme.ts'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
